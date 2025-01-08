ac520c94d6dc9ae82c3d1edba232e65d1877713b
Andreas Bucher <andreas.bucher@gmx.ch>
2025-01-08 11:08:50 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 # Start crond in the background
 crond -f &
 
-# Start the MQTT shell script in the background
-/bin/bash /opt/nanohome/services/mqtt_shell -s &
-/bin/bash /opt/nanohome/driver/shelly_plus_devwatcher &
-/bin/bash /opt/nanohome/driver/shelly_plus_measurements & 
-/bin/bash /opt/nanohome/driver/shelly_plus_standby &
-/bin/bash /opt/nanohome/driver/shelly_legacy_devwatcher &
-/bin/bash /opt/nanohome/driver/shelly_legacy_measurements &
+# Start nanohome services
+#/bin/bash /opt/nanohome/services/mqtt_shell -s &
+#/bin/bash /opt/nanohome/driver/shelly_plus_devwatcher &
+#/bin/bash /opt/nanohome/driver/shelly_plus_measurements & 
+#/bin/bash /opt/nanohome/driver/shelly_plus_standby &
+#/bin/bash /opt/nanohome/driver/shelly_legacy_devwatcher &
+#/bin/bash /opt/nanohome/driver/shelly_legacy_measurements &
 
 # Start bash
 exec bash
