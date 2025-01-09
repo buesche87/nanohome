cf86491cdde462f65a6b4beaab795714fc85c64d
Andreas Bucher <andreas.bucher@gmx.ch>
2025-01-09 23:05:41 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -34,10 +34,10 @@ crond -f &
 # Start nanohome services
 /bin/bash /opt/nanohome/services/mqtt_shell -s &
 /bin/bash /opt/nanohome/services/devwatcher_shelly_legacy &
-#/bin/bash /opt/nanohome/services/devwatcher_shelly_plus & 
+/bin/bash /opt/nanohome/services/devwatcher_shelly_plus &
 /bin/bash /opt/nanohome/services/measurements_shelly_legacy &
-#/bin/bash /opt/nanohome/services/measurements_shelly_plus &
-#/bin/bash /opt/nanohome/standby_shelly_plus &
+/bin/bash /opt/nanohome/services/measurements_shelly_plus &
+#/bin/bash /opt/nanohome/services/standby_shelly_plus &
 
 # Start bash
 exec bash
