e1cc546610853bae8103c46bd92bc6609ea4d8a8
Andreas Bucher <andreas.bucher@gmx.ch>
2025-02-10 11:01:42 +0100

    merge plus services

--- start.sh
+++ start.sh
@@ -1123,7 +1123,7 @@ crond -f &
 # i.O.
 /bin/bash ${NANOHOME_ROOTPATH}/services/nanohome_shell &
 /bin/bash ${NANOHOME_ROOTPATH}/services/devwatcher_shelly_legacy &
-/bin/bash ${NANOHOME_ROOTPATH}/services/devwatcher_shelly_plus &
+# /bin/bash ${NANOHOME_ROOTPATH}/services/devwatcher_shelly_plus &
 /bin/bash ${NANOHOME_ROOTPATH}/services/measurements_shelly_legacy &
 /bin/bash ${NANOHOME_ROOTPATH}/services/measurements_shelly_plus &
 #/bin/bash ${NANOHOME_ROOTPATH}/bin/create_standbymgr
